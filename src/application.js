cda8d1027cd65683a875212ab8faedace1d2751b
Paul Holden <pholden@stria.com>
2019-10-04 15:01:55 -0700

    More buildout, fix up config help

--- src/application.js
+++ src/application.js
@@ -1,10 +1,15 @@
 // Require tedious so it is included in pkg build
 require('tedious');
 
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
 const rc = require('rc');
+const async = require('async');
+const moment = require('moment');
 
 const generateConfigHelp = require('./generate-config-help.js');
+const models = require('./models.js');
 
 class Application {
   constructor(appName) {
@@ -38,7 +43,67 @@ class Application {
   // Locate and process Oracle extracts in the designated drop folder
   // in ascending chronological order.
   processConfigs() {
-    // stub
+    const fileRegexp = /^PMH_Stria_EE_Data_(\d{14})\.csv$/;
+
+    let actions = {};
+    let promise = new Promise((resolve, reject) => {
+      actions.resolve = resolve;
+      actions.reject = reject;
+    });
+
+    const baseDir = this.config.fileDropFolder;
+
+    fs.readdir(baseDir, (err, list) => {
+      if (err) {
+        return actions.reject(err);
+      }
+
+      // Filter out non-CSV files
+      let fileList = list.filter(file => {
+        let match = file.match(fileRegexp);
+
+        if (!match) {
+          return false;
+        }
+
+        const filePath = path.join(baseDir, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+          return false;
+        }
+
+        return true;
+      });
+
+      // Sort files by timestamp in name
+      fileList = fileList.map(f => {
+        let match = f.match(fileRegexp);
+
+        let m = moment(match[1], 'YYYYMMDDHHmmss');
+
+        return {
+          moment: m,
+          fileName: f
+        };
+      }).sort((a, b) => {
+        return b.moment.isBefore(a.moment);
+      }).map(f => f.fileName);
+
+      async.eachSeries(fileList, async (fileName) => {
+        return this.processExtract(fileName);
+      }).then(actions.resolve).catch(actions.reject);
+    });
+
+    return promise;
+  }
+
+  processExtract(fileName) {
+    const baseDir = this.config.fileDropFolder;
+    const filePath = path.join(baseDir, fileName);
+
+    console.log(`Processing ${filePath}`);
+
     return Promise.resolve();
   }
 }
--- src/generate-config-help.js
+++ src/generate-config-help.js
@@ -17,19 +17,42 @@ Data is merged down; earlier configs override those that follow.
 Example configuration (JSON):
   {
       "mssql": {
-        "hostname": "<DB hostname>"
-        "username": "<DB username>"
-        "password": "<DB password>"
-        "database": "<DB database name>"
+        "hostname": "<DB hostname>",
+        "username": "<DB username>",
+        "password": "<DB password>",
+        "database": "<DB database name>",
         "table": "<DB master table name>"
-      }
+      },
+      "mappingColumn": "Business Unit",
+      "hospitalMapping": {
+        "<BusinessUnit Value>": {
+          "outputPath": "/Where/File/Goes/In/SpringCM",
+          "columnMapping": [
+            {
+              "master": "<Master table column name>",
+              "target": "<Output CSV column name>"
+            },
+            {
+              "master": "<Master table column name>",
+              "target": "<Output CSV column name>"
+            }
+          ]
+        }
+      },
       "fileDropFolder": "\\\\storage1\\folder\\path"
     }
 
-About configuration settings
+About configuration settings:
  - mssql.*: Microsoft SQL Server connection info.
  - mssql.table: The master table where all data will be stored. Data is
                 collected here via upsert by employee ID.
+ - mappingColumn: Name of the column used to split and map output CSVs
+ - hospitalMapping.*: Configuration hash for a particular hospital. The key
+                      is the hospital mapping column value, e.g. CROZER
+ - hospitalMapping.<hospital>.outputPath: Where the output CSV is uploaded
+ - hospitalMapping.<hospital>.columnMapping: Array of mapped columns
+    - ...columnMapping[].master: Column name in master database table
+    - ...columnMapping[].target: Output CSV column name
  - fileDropFolder: Where the nightly Oracle HRIS extracts will land.
 `;
 }
